01d4bc5c7c33f8c3358f04a58be6198f27ae6f55
GabriellaQiong <qiong@seas.upenn.edu>
2014-04-02 23:27:19 -0400

    add preprocessing

--- code/run_script.m
+++ code/run_script.m
@@ -13,6 +13,10 @@ verbose = true;                  % Whether to show the details
 train   = true;                  % Whether to train
 test    = false;                 % Whether to test
 
+% Params
+featNum = 5;                     % Number of features
+H       = cell(featNum, 1);
+
 %% Paths
 scriptDir  = fileparts(mfilename('fullpath'));
 outputDir  = fullfile(scriptDir, '../results');
@@ -21,57 +25,22 @@ if ~exist(outputDir, 'dir')
 end
 addpath(genpath(scriptDir));
 
-%% Load data
-map = imread('aerial_color.jpg');
-C   = makecform('srgb2lab');
-lab = applycform(map, C);
-vec = reshape(lab, [], 3);
+%% Load Data and Preprocess Data
+% Load original map
+orig = imread('aerial_color.jpg');
+
+% Preprocess map
+% map  = medfilt2(orig);
+map  = orig;
+se   = strel('ball', 5, 5);
+map  = imdilate(map, se);
+map  = imerode(map, se);
+
+% Map data into Lab space
+C    = makecform('srgb2lab');
+lab  = applycform(map, C);
+as   = lab(:, :, 2);
+bs   = lab(:, :, 3);
         
-%% Parse data
-[dc, alpha, tsEn]  = parse_encoders(Encoders);
-[tilt, rpy, tsImu] = parse_imu(Imu, verbose);
-indices            = sync_time(tsImu, tsEn);
-rpy                = rpy(:, indices);
-theta              = angle_fuse(alpha, rpy(3, :), tsEn, verbose);
+%% Train
 
-%% Dead Reckoning
-% Initialize
-[x, y, theta] = dead_reckoning(dc, alpha, theta, tsEn, verbose);
-
-% Sync the time
-tsHo   = Hokuyo.ts;
-ranges = Hokuyo.ranges;
-angles = Hokuyo.angles;
-
-if length(tsEn) > length(tsHo)
-    ts  = tsHo;
-    ind = sync_time(tsEn, ts);
-    x   = x(ind);
-    y   = y(ind);
-    rpy = rpy(:, ind);
-else
-    ts     = tsEn;
-    ind    = sync_time(tsHo, ts);
-    ranges = ranges(:, ind);
-end
-numData = length(ts);
-
-% Test the results
-if check
-    testMapCorrelation
-end
-
-%% SLAM
-MCL_SLAM;
-
-%% Integrate Kinect data
-if ~kinect
-    return;
-end
-
-% Initialize and load data
-cMap = zeros(size(MAP.map), 3);
-k    = load(fullfile(dataDir, ['kinect', num2str(dataIdx)]));
-
-% Process the kinect data
-process_kinect;
